87875e8e31457c4f2627b035ca406b0ecc8a5552
seolDong <dh10513@gmail.com>
2022-11-08 01:22:23 +0900

    mainPage_update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ export function navColorObserver(target) {
       if (entry.isIntersecting == true) {
         nav.style.backgroundColor = "transparent";
       } else {
-        nav.style.backgroundColor = `rgb(175, 96, 0)`;
+        nav.style.backgroundColor = `black`;
       }
     });
   };
@@ -35,14 +35,37 @@ navColorObserver(contact)
 
 const slideList = [...document.querySelectorAll(".slideList")];
 const slideArr = slideList.map((v) => v.classList[1]);
+console.log(slideList);
 
 function slideTimer() {
-  let count = 1;
+  let count = 0;
+
+  const leftBtn = document.querySelector('.leftBtn')
+  const rightBtn = document.querySelector('.rightBtn')
+
+  leftBtn.addEventListener('click', () => {
+    slideList.forEach((v) => { v.classList.remove(`active`); });
+    if (count <= 0) { count = 1; }
+    count--
+    const target = document.querySelector(`.${slideArr[count]}`);
+    target.classList.add(`active`);
+    console.log(count);
+  })
+
+  rightBtn.addEventListener('click', () => {
+    slideList.forEach((v) => { v.classList.remove(`active`); });
+    if (count >= slideList.length) { count = 6 }
+    const target = document.querySelector(`.${slideArr[count]}`);
+    target.classList.add(`active`);
+    console.log(count);
+  })
+
   setInterval(() => {
     if (count >= slideList.length) { count = 0; }
     slideList.forEach((v) => { v.classList.remove(`active`); });
     const target = document.querySelector(`.${slideArr[count]}`);
     target.classList.add(`active`);
+
     count++;
   }, 3000);
 }
@@ -51,18 +74,23 @@ slideTimer();
 
 //
 
-// view my work BTN
 
-const nextBtnText = document.querySelector(`.nextBtnText`);
-const introduce = document.querySelector(`#introduce`);
 
-const viewBtnCallback = ()=>{
-  introduce.scrollIntoView({behavior: 'smooth'});
+for(let entry in slideList){
+  console.log(entry);
 }
 
-nextBtnText.addEventListener('click', viewBtnCallback)
+
+// view my work BTN
+
+const main_nextBtn = document.querySelector(`.main_nextBtn`);
+const line1 = document.querySelector(`.line1`);
+
+const viewBtnCallback = () => {
+  line1.scrollIntoView({ behavior: 'smooth' });
+}
+
+main_nextBtn.addEventListener('click', viewBtnCallback)
 
 // 
-
-
 
