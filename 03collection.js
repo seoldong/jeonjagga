8bd651ecaff9d1f9091c8b023f90efc4e5499c09
seolDong <dh10513@gmail.com>
2022-11-09 16:32:51 +0900

    page_all_1.0

--- 03collection.js
+++ 03collection.js
@@ -1,30 +1,4 @@
 "use strict";
 
-// import { nav, navColorObserver } from './01index'
+const instaTest = document.querySelector('instaTest');
 
-//nav color
-
-const main = document.querySelector(`#main`);
-
-export const nav = document.querySelector(`#nav`);
-export function navColorObserver(target) {
-  const option = { root: null, rootMargin: `0px`, threshold: 0.8 };
-
-  const callback = (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting == true) {
-        nav.style.backgroundColor = "transparent";
-      } else {
-        nav.style.backgroundColor = `black`;
-      }
-    });
-  };
-
-  const observer = new IntersectionObserver(callback, option);
-
-  return observer.observe(target)
-}
-
-navColorObserver(main)
-
-// 
